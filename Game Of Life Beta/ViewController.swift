fb867b5e3e7622f6c27738cb3038a1d3eec0530f
Ido Tamir <idotamir516@gmail.com>
2016-11-07 11:43:49 -0500

    Initial Commit

---
+++ Game Of Life Beta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Of Life Beta
+//
+//  Created by Ido Tamir on 11/7/16.
+//  Copyright © 2016 Ido Tamir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
